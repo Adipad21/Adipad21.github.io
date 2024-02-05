abe7a898c5896b97bc403a603b75640b82b9e89a
Adipad21 <aditya.padala213@gmail.com>
2024-02-04 22:14:56 -0500

    added index.html

--- csce242/projects/part3/script.js
+++ csce242/projects/part3/script.js
@@ -1,9 +1,14 @@
-// Toggles the nav items to show/hide when the hamburger menu is clicked
-const toggleHamburger = () => {
-    document.getElementById("columns").classList.toggle("hide");
-  };
-  
-  // Calls the toggleHamburger function when the hamburger menu is clicked
-  window.onload = () => {
-    document.getElementsById("container").onclick = toggleHamburger;
-  };
+  document.addEventListener('DOMContentLoaded', function() {
+    var menuContainer = document.getElementById('container');
+    var navItems = document.getElementById('columns');
+
+    menuContainer.addEventListener('click', function() {
+        navItems.classList.toggle('active');
+    });
+});
+window.addEventListener('resize', function() {
+  var navItems = document.getElementById('columns');
+  if (window.innerWidth >= 800) {
+      navItems.classList.remove('active');
+  }
+});
