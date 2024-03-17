437eb92b375eff38b6d3faa1823f2598cb982727
Adipad21 <aditya.padala213@gmail.com>
2024-03-16 20:32:57 -0400

    added index.html

--- csce242/projects/part5/script.js
+++ csce242/projects/part5/script.js
@@ -1,14 +1,24 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    var menuContainer = document.getElementById('container');
-    var navItems = document.getElementById('columns');
+  document.addEventListener('DOMContentLoaded', () => {
+    const menuContainer = document.getElementById('container');
+    const navItems = document.getElementById('columns');
 
-    menuContainer.addEventListener('click', function() {
+    menuContainer.addEventListener('click', () => {
         navItems.classList.toggle('active');
     });
 });
-window.addEventListener('resize', function() {
-  var navItems = document.getElementById('columns');
+window.addEventListener('resize', () => {
+  const navItems = document.getElementById('columns');
   if (window.innerWidth >= 800) {
       navItems.classList.remove('active');
+  }
+});
+
+const registerForm = document.getElementById("register");
+document.getElementById("register-link").addEventListener('click', (event) => {
+  event.preventDefault();   
+  if (registerForm.style.display === 'none') {
+      registerForm.style.display = 'block';
+  } else {
+      registerForm.style.display = 'none';
   }
 });
