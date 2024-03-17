ec92e681dc7009228857288472bf9959122aaddf
Adipad21 <aditya.padala213@gmail.com>
2024-03-17 11:10:12 -0400

    added index.html

--- csce242/projects/part5/script.js
+++ csce242/projects/part5/script.js
@@ -1,24 +1,19 @@
-  document.addEventListener('DOMContentLoaded', () => {
-    const menuContainer = document.getElementById('container');
-    const navItems = document.getElementById('columns');
+  document.addEventListener("DOMContentLoaded", () => {
+    const menuContainer = document.getElementById("container");
+    const navItems = document.getElementById("columns");
 
-    menuContainer.addEventListener('click', () => {
-        navItems.classList.toggle('active');
+    menuContainer.addEventListener("click", () => {
+        navItems.classList.toggle("active");
     });
 });
-window.addEventListener('resize', () => {
-  const navItems = document.getElementById('columns');
+window.addEventListener("resize", () => {
+  const navItems = document.getElementById("columns");
   if (window.innerWidth >= 800) {
-      navItems.classList.remove('active');
+      navItems.classList.remove("active");
   }
 });
 
-const registerForm = document.getElementById("register");
-document.getElementById("register-link").addEventListener('click', (event) => {
-  event.preventDefault();   
-  if (registerForm.style.display === 'none') {
-      registerForm.style.display = 'block';
-  } else {
-      registerForm.style.display = 'none';
-  }
+document.getElementById("register-link").addEventListener("click", (event) => {
+  event.preventDefault();
+  document.getElementById("register").classList.toggle("hidden");
 });
